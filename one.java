bdfaeae66019e154e320965ae23719508fb3900e
Nandhini325 <nandhinirajendran33@gmail.com>
2024-04-17 21:35:50 +0530

    one.java

---
+++ one.java
@@ -0,0 +1,11 @@
+ class one{
+    public static void main(String args[])
+    {
+        int softskill = 1;
+        String softskill1 = "CONSISTENCY PATIENCE HOPE ";
+        System.out.println(softskill);
+        System.out.println(softskill1);
+    }
+}
+    
+
