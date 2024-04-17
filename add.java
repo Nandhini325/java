1e8e2be287932d45338f754c34053c41521a4716
Nandhini325 <nandhinirajendran33@gmail.com>
2024-04-17 21:48:26 +0530

    add.java

---
+++ add.java
@@ -0,0 +1,15 @@
+class add{
+    public static void main(String args[])
+    {
+        int x = 100;
+        int y = 100;
+        int z = 100;
+        System.out.println(x+y+z);
+    }
+}
+    
+
+    
+
+    
+
