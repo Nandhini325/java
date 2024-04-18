4746caa4a18d6e9bde7b0473cf4ec85f1b524705
Nandhini325 <nandhinirajendran33@gmail.com>
2024-04-18 21:49:09 +0530

    test1.java

---
+++ test1.java
@@ -0,0 +1,13 @@
+class test1{
+    static public void main(String args[])
+ {
+    String name = "Thiya";
+    int age =7;
+    System.out.println( "MY NAME IS  " +  name);
+    System.out.println("MY AGE IS  " +   age);
+ }
+}
+
+
+
+ 
