91cb228d8b06b07936437ec6ba06590fa676bea9
Nandhini325 <nandhinirajendran33@gmail.com>
2024-04-18 21:51:11 +0530

    test2.java

---
+++ test2.java
@@ -0,0 +1,10 @@
+class test2{
+ static public void main(String args[])
+ {
+    int a,b,c;           // int is datatype
+    a = b = c = 10;
+    int d =(a*b*c);      // * is a operator,it perform multiple operation
+    int e =(a+b+c);
+    float f =(d/e);        // / is a operator it perform division operation
+    System.out.println(f);    
+}}
