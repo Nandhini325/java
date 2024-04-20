9b8caed0f5a42af55d05b65e6cb4b18edec74e48
Nandhini325 <nandhinirajendran33@gmail.com>
2024-04-20 20:50:31 +0530

    test3.java

---
+++ test3.java
@@ -0,0 +1,14 @@
+import java.lang.System;       
+import java.util.Scanner;      
+class test3{
+    public static void main(String args[])
+    {
+        Scanner quote = new Scanner(System.in); 
+        int x = quote.nextInt();
+        int y = quote.nextInt();
+        System.out.print(x+y);    }
+} 
+
+
+
+ 
