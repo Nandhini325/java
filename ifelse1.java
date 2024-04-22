85c277800f423c0a9022ce2ab18bd60c8799318b
Nandhini325 <nandhinirajendran33@gmail.com>
2024-04-22 21:54:14 +0530

    ifelse1.java

---
+++ ifelse1.java
@@ -0,0 +1,15 @@
+ class ifelse1{
+    public static void main(String args[])
+    {
+        boolean study = true;
+        if(study)
+        {
+             System.out.println("Definitely you will achieve your goal");
+        }     
+        else
+        {
+            System.out.print("Nothing to say");
+        }     
+                 
+    }
+}   
